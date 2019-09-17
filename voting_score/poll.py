60d0c59faec4297ca98ba6dd9aaa608e5dec677c
Tomaz Mesarec <tomaz.mesarec@block42.tech>
2019-09-17 10:11:36 +0200

    Only saving voters choice, without the current balance of his wallet

--- voting_score/poll.py
+++ voting_score/poll.py
@@ -8,7 +8,7 @@ class Poll:
         self.__answers = self.addAnswers(obj['answers'])
         self.__timestamp = obj['timestamp']
         self.__initiator = obj['initiator']
-        self.__votes = dict() if 'votes' not in obj else obj['votes']
+        self.__voters = dict() if 'voters' not in obj else obj['voters']
 
     def addAnswers(self, answers: list) -> list:
         temp_list = []
@@ -16,22 +16,11 @@ class Poll:
             new_answer = dict()
             new_answer["id"] = len(temp_list) if 'id' not in ans else ans['id']
             new_answer["name"] = ans if 'name' not in ans else ans['name']
-            new_answer["votes"] = 0 if 'votes' not in ans else ans['votes']
             temp_list.append(new_answer)
         return temp_list
 
     def vote(self, answer_id: int, sender_balance: int, sender_address: str) -> None:
-        # if voter already voted previously
-        if sender_address in self.__votes:
-            # substract previous vote
-            prev_answer_tuple = next(iter(self.__votes[sender_address].items()))
-            prev_answer = self.getAnswerById(prev_answer_tuple[0])
-            prev_answer['votes'] = prev_answer['votes'] - prev_answer_tuple[1]
-        # add a new vote
-        answer = self.getAnswerById(answer_id)
-        answer["votes"] = answer["votes"] + sender_balance
-        self.__votes[sender_address] = {answer_id: sender_balance}
-
+        self.__voters[sender_address] = answer_id;
 
     def getId(self) -> int:
         return self.__id
@@ -55,7 +44,7 @@ class Poll:
                 "answers": self.__answers,
                 "timestamp": self.__timestamp,
                 "initiator": self.__initiator,
-                "votes": self.__votes,
+                "voters": self.__voters,
                 }
 
     @staticmethod
